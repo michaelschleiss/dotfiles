fce52bc12ae90fadf5a2a02671e2f666b3296279
michaelschleiss <michaelschleiss@gmx.de>
2025-05-23 09:58:55 +0200

    add yaml to ensure installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			-- ensure_installed = { "lua", "python", "c", "cpp", "markdown" },
+			ensure_installed = { "lua", "python", "c", "cpp", "markdown", "yaml" },
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
