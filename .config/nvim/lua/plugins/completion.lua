7be0ade40adaac3d59905dfcb9cb2d6fd6a8c23a
michaelschleiss <michaelschleiss@gmx.de>
2025-08-06 08:02:30 +0200

    update vimtex

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,6 +1,12 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
+    "hrsh7th/cmp-nvim-lsp",
+      dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-omni",
+        "kdheepak/cmp-latex-symbols",
+  },
   },
   {
     "L3MON4D3/LuaSnip",
@@ -10,37 +16,39 @@ return {
     },
   },
   {
-  "hrsh7th/nvim-cmp",
-  config = function()
-    local cmp = require("cmp")
-    require("luasnip.loaders.from_vscode").lazy_load()
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-    cmp.setup({
-      snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-        -- Keymaps
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = 'luasnip' }, -- For luasnip users.
-      }, {
-        { name = "buffer" },
-      }),
-    })
-  end,
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          -- Keymaps
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = 'luasnip' }, -- For luasnip users.
+          { name = "buffer" },
+          { name = "omni" }, -- For vimtex
+          { name = "latex_symbols" }, -- For vimtex
+          { name = "path" }, -- For vimtex
+        }),
+      })
+    end,
   }
 }
