3007310411807504c33980412fa0ea39771fc017
michaelschleiss <michaelschleiss@gmx.de>
2025-08-06 08:02:19 +0200

    update treesiter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,17 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
+ 	   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.latex = {
+  install_info = {
+    url = "https://github.com/latex-lsp/tree-sitter-latex",
+    files = { "src/grammar.json" }, -- not strictly necessary, but a fallback
+    generate_requires_npm = true, -- needed because it uses grammar.js
+    requires_generate_from_grammar = true,
+  },
+  filetype = "tex",
+}
+
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "lua", "python", "c", "cpp", "markdown", "yaml" },
 			auto_install = true,
