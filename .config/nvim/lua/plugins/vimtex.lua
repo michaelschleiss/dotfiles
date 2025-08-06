a6e8bf342fefa2f2747ca11d98916867897f31b2
michaelschleiss <michaelschleiss@gmx.de>
2025-08-06 08:02:13 +0200

    update vimtex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,18 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_activate = 1
+    vim.g.vimtex_compiler_latexmk = {
+      out_dir = "build",
+      options = {
+        "-verbose",
+        "-file-line-error",
+        "-synctex=1",
+        "-interaction=nonstopmode",
+        "-shell-escape",
+      },
+    }
   end
 }
